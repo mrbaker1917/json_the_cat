600f58f6627fa7184f7958b0959acc40e27feead
Mark Baker <mrbaker1917@gmail.com>
2020-08-27 21:27:24 +0000

    refactored app with index.js and added tests in breedFetcherTest.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,17 @@
 const request = require('request');
-const args = process.argv.slice(2);
-let searchAddress = 'https://api.thecatapi.com/v1/breeds/search?q=' + args[0];
-request(searchAddress, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else if (body === '[]') {
-    console.log("Sorry, that breed was not found. Try another one!");
-  } else {
-    const data = JSON.parse(body)[0];
-    console.log(`Name: ${data.name}`);
-    console.log(`Description: ${data.description}`);
-  }
-});
+
+const fetchBreedDescription = function(breedName, callback) {
+  let searchAddress = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  request(searchAddress, (error, response, body) => {
+    let description = "";
+    if (body === '[]') {
+      description = "Sorry, that breed was not found. Try another one!";
+    } else {
+      const data = JSON.parse(body)[0];
+      description = data.description;
+    }
+    callback(error, description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
