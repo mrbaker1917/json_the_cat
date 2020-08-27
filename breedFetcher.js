2758601275ad191fbdf352555109c1fda8f55ee0
Mark Baker <mrbaker1917@gmail.com>
2020-08-27 20:00:43 +0000

    Adds working breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,14 @@
 const request = require('request');
+const args = process.argv.slice(2);
+let searchAddress = 'https://api.thecatapi.com/v1/breeds/search?q=' + args[0];
+request(searchAddress, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else if (body === '[]') {
+    console.log("Sorry, that breed was not found. Try another one!");
+  } else {
+    const data = JSON.parse(body)[0];
+    console.log(`Name: ${data.name}`);
+    console.log(`Description: ${data.description}`);
+  }
+});
